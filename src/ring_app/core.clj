f3afc5f47f980e22777fa6d75f3cd61415b4e46d
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-01-16 18:09:26 +0000

    introduce first middleware function to add pragma no-cache header

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -8,8 +8,15 @@
               (:remote-addr request-map)
               "</body></html>")})
 
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
 (defn -main []
   (jetty/run-jetty
-    handler
+    (-> handler
+        wrap-nocache)
     {:port 3000
      :join? false}))
