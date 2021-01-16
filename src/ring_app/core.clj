18f60d8677a8ba455d1a386f063e8ae5e06e2b98
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-01-16 18:44:04 +0000

    add new json-handler function that returns a map as body of the response. add middleware function `wrap-formats` and trial it with curl commands

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,13 +1,20 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
-            [ring.util.http-response :as response]))
+            [ring.util.http-response :as response]
+            [muuntaja.middleware :as muuntaja]))
 
-(defn handler [request-map]
+(defn html-handler [request-map]
   (response/ok
     (str "<html><body> Hello, your IP is: "
          (:remote-addr request-map)
          "</body></html>")))
+
+(defn json-handler [request]
+  (response/ok
+    {:result (get-in request [:body-params :id])}))
+
+(def handler json-handler)
 
 (defn wrap-nocache [handler]
   (fn [request]
@@ -15,10 +22,19 @@
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
 
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
 (defn -main []
   (jetty/run-jetty
     (-> #'handler
         wrap-nocache
-        wrap-reload)
+        wrap-reload
+        wrap-formats)
     {:port  3000
      :join? false}))
+
+; curl -I localhost:3000
+; curl localhost:3000
+; curl -H "Content-Type: application/json" -X POST -d '{"id":1}' localhost:3000
