57d2fa73ba1efa52b8189509347324a399660ad0
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-01-16 17:49:42 +0000

    create handler function that return a response

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,5 +1,11 @@
 (ns ring-app.core)
 
+(defn handler [request-map]
+  {:status 200
+   :headers {"Content/type" "text/html"}
+   :body (str "<html><body> your IP is: "
+              (:remote-addr request-map)
+              "</body></html>")})
 (defn foo
   "I don't do a whole lot."
   [x]
