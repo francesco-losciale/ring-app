5393bee1c8116548f9766c21ab9c30ec49625463
Francesco Losciale <francesco.losciale@thoughtworks.com>
2021-01-17 22:46:09 +0000

    move wrap-format middleware up, create default routes for http errors

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -15,6 +15,10 @@
   (response/ok
     {:result (get-in request [:body-params :id])}))
 
+(defn wrap-formats [handler]
+  (-> handler
+      (muuntaja/wrap-format)))
+
 (def routes
   [["/" {:get  html-handler
          :post html-handler}]
@@ -30,18 +34,22 @@
         (response/ok {:result (* a b)}))}]]])
 
 (def handler
-  (reitit/ring-handler
-    (reitit/router routes)))
+  (reitit/routes
+    (reitit/ring-handler
+     (reitit/router routes))
+    (reitit/create-default-handler
+      {:not-found
+       (constantly (response/not-found "404 - Not Found"))
+       :method-not-allowed
+       (constantly (response/method-not-allowed "405 - Method not allowed"))
+       :not-acceptable
+       (constantly (response/not-acceptable "406 - Not acceptable"))})))
 
 (defn wrap-nocache [handler]
   (fn [request]
     (-> request
         handler
         (assoc-in [:headers "Pragma"] "no-cache"))))
-
-(defn wrap-formats [handler]
-  (-> handler
-      (muuntaja/wrap-format)))
 
 (defn -main []
   (jetty/run-jetty
